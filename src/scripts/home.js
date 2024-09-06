feab5963505546d46b7934d8679deb6d873d36f5
Sho8000 <yoshikichi8000@gmail.com>
2024-09-07 08:16:11 +0900

    Home Animation

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,11 +1,39 @@
 $(() => {
 
-//  gsap.to(".char", {
-//    y: 0 /*テキストのY軸の操作*/,
-//    stagger: 0.05 /*テキスト間の遅延時間*/,
-//    delay: 0.2 /*アニメーションのスタートまでの遅延時間*/,
-//    duration: 0.5 /*アニメーションの時間*/,
-//    ease: power2.out /*イージングの設定*/,
-//  });
+  $(".Courgette").on("click",function(){
+    console.log("Hello")
+    })
+
+  $(".ForLink").on( "mouseenter", ()=>{
+    $("#MainPicture").toggleClass("FilterBrightnessON")
+    $(".LetterContainer").toggleClass("FilterBrightnessON")
+  } ).on( "mouseleave", ()=>{
+    $("#MainPicture").toggleClass("FilterBrightnessON")
+    $(".LetterContainer").toggleClass("FilterBrightnessON")
+  });
+
+  $(".LinkLogo").on("mouseenter", function(){
+    $(this).toggleClass("FilterBrightnessON")
+  }).on( "mouseleave", function(){
+    $(this).toggleClass("FilterBrightnessON")
+  })
+
+
+  var tlFade = gsap.timeline();
+
+  tlFade.from(".Fade", {
+    scaleY: 0,
+    stagger: 0.2,
+    duration: 0.5,
+  });
+
+  $(".MoveToOtherPages").on("click",function(){
+    tlFade.to(".Fade", {
+      scaleY: 0,
+      stagger: 0.2,
+      duration: 0.5,
+    });
   
+  })
+
 })
