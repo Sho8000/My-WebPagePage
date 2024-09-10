19d548a697d11857a4c8e6f526d00e28c4d964a1
Sho8000 <yoshikichi8000@gmail.com>
2024-09-11 03:58:56 +0900

    ProjectPage

---
+++ src/scripts/project.js
@@ -0,0 +1,28 @@
+$(() => {
+
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    effect: "cube",
+    grabCursor: true,
+    cubeEffect: {
+      shadow:true,
+      slideShadows:true,
+      shadowOffset:20,
+      shadowScale:0.94,
+    },
+    loop: true,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+})
